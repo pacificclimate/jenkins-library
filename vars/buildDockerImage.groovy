941058c9388cd44e80bf95d9333c0f925a5b3d14
Nikola Rados <nrados95@gmail.com>
2019-12-06 14:42:07 -0800

    Change default name to all lowercase

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,4 +1,4 @@
-def call(String image_name='myImage') {
+def call(String image_name='my-image') {
     def image
     withDockerServer([uri: PCIC_DOCKER]) {
         image = docker.build(image_name)
