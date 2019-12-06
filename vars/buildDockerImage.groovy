fdaf8e8f7f6f0a85c02159ae2e5c0f3d136dc64a
Nikola Rados <nrados95@gmail.com>
2019-12-06 15:25:10 -0800

    Update docstring

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,10 +1,12 @@
 /**
- * Build a docker image given the name
+ * Build a docker image given the name and server URI.  By default this function
+ * will build this image on docker-dev01.
  *
  * @param image_name the name of the image
+ * @param server_uri URI of the docker server to build on
  * @return image the built docker image
  */
-def call(String image_name='my-image', server_uri=PCIC_DOCKER) {
+def call(String image_name='my-image', server_uri=PCIC_DOCKER_DEV01) {
     def image
     withDockerServer([uri: server_uri]) {
         image = docker.build(image_name)
