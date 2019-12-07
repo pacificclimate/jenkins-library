c3fce991f8e71ff40ff00dcc7ed65f0fb1a6ab3b
Nikola Rados <nrados95@gmail.com>
2019-12-06 16:51:02 -0800

    Make docker registry a parameter

--- vars/publishDockerImage.groovy
+++ vars/publishDockerImage.groovy
@@ -4,10 +4,11 @@
  * @param image to publish
  * @param tags list of tags to publish image with
  * @param server_uri URI of the docker server to build on
+ * @param registry_url URL of a registry
  */
-def call(image, ArrayList tags, String server_uri = PCIC_DOCKER_DEV01) {
+def call(image, ArrayList tags, String server_uri = PCIC_DOCKER_DEV01, String registry_url = '') {
     withDockerServer([uri: server_uri]){
-        docker.withRegistry('', 'PCIC_DOCKERHUB_CREDS') {
+        docker.withRegistry(registry_url, 'PCIC_DOCKERHUB_CREDS') {
             tags.each { tag ->
                 image.push(tag)
             }
