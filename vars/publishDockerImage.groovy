1d02ba071704ee45a86b77b53d96fb84d0b41776
Nikola Rados <nrados95@gmail.com>
2019-12-06 16:20:28 -0800

    Remove credential id from params

--- vars/publishDockerImage.groovy
+++ vars/publishDockerImage.groovy
@@ -4,11 +4,10 @@
  * @param image to publish
  * @param tags list of tags to publish image with
  * @param server_uri URI of the docker server to build on
- * @param credentials jekins credential for docker registry
  */
-def call(image, ArrayList tags, String server_uri = PCIC_DOCKER_DEV01, credentials = PCIC_DOCKERHUB_CREDS) {
+def call(image, ArrayList tags, String server_uri = PCIC_DOCKER_DEV01) {
     withDockerServer([uri: server_uri]){
-        docker.withRegistry('', credentials) {
+        docker.withRegistry('', 'PCIC_DOCKERHUB_CREDS') {
             tags.each { tag ->
                 image.push(tag)
             }
