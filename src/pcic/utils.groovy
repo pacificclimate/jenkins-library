b9d4aa02a00ee73f0aca3b6a54869eaa77308b5e
Nikola Rados <nrados95@gmail.com>
2019-12-11 16:09:11 -0800

    Add utility package

---
+++ src/pcic/utils.groovy
@@ -0,0 +1,24 @@
+package pcic
+
+
+def getPipString(int pythonVersion) {
+    String pip
+    if (pythonVersion == 3) {
+        pip = 'pip3'
+    } else {
+        pip = 'pip'
+    }
+
+    return pip
+}
+
+
+def applyOptionalParameters(Map defaultMap, Map newMap) {
+    def unknownKeys = (defaultMap.keySet() + newMap.keySet()) - defaultMap.keySet()
+
+    if (unknownKeys.size() > 0) {
+        throw new Exception("Key(s) ${unknownKeys} not available in ${defaultMap.keySet()}")
+    }
+
+    return defaultMap + newMap
+}
