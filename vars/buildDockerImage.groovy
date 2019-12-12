352bcd04e59f2215cb2c5426889b3b908e11aeac
Nikola Rados <nrados95@gmail.com>
2019-12-11 16:06:49 -0800

    Remove extra spacing

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -8,10 +8,8 @@
  */
 def call(String image_name, String server_uri = PCIC_DOCKER_DEV01) {
     def image
-
     withDockerServer([uri: server_uri]) {
         image = docker.build(image_name)
     }
-
     return image
 }
